75d117b9873ad14a03325d0b21eee5a393e264d4
sravanib04 <sbham3@uic.edu>
2025-11-09 17:53:44 -0600

    Problem 1

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,8 +1,17 @@
 from typing import List
-"""
-Intuition: Sort the array and see if the previous number is the same as the correct number. TC: O(n log n) SC: O(n) or O(1) depending on the sorting algorithm
 
-Approach: Use a hash set to store the numbers that are already seen. Return true if the number is seen already. Return False at the end because it means that the no duplicates are found. TC: O(n) SC: O(n)
+"""
+Intuition: 
+    Sort the array and see if the previous number is the same as the correct number. 
+    TC: O(n log n) 
+    SC: O(n) or O(1) depending on the sorting algorithm
+
+Approach: 
+    Use a hash set to store the numbers that are already seen. 
+    Return true if the number is seen already. 
+    Return False at the end because it means that the no duplicates are found. 
+    TC: O(n) 
+    SC: O(n)
 """
 
 def hasDuplicate(nums: List[int]) -> bool:
